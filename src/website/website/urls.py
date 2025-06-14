4e95df271f48e6171815786be7c61095db18d318
Jordan Morgan <jordan.morgan999@gmail.com>
2025-06-13 22:10:38 -0600

    got chatbot app connected to openAI api

--- src/website/website/urls.py
+++ src/website/website/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),  # Delegates to main/urls.py
+    path('', include('main.urls')),
+    path('api/', include('chatbot.urls')),
 ]
